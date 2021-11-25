3123d40beb52e22db1c8eeb46d453de909284747
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-25 14:09:10 +0100

    esercizio

---
+++ script.js
@@ -0,0 +1,8 @@
+var app = new Vue (
+    {
+    el: '#root',
+    data: {
+        message: "Hello World",
+        mioPath: "https://www.turismoroma.it/sites/default/files/Roma%20in%20breve.jpg"
+    }
+})
